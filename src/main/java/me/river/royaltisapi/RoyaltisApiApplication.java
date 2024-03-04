ee5ac95843c1a45b070e367a034459efa9c7623c
riveerxd <lukas.hrehor@gmail.com>
2024-03-04 13:48:38 +0100

    initial commit

---
+++ src/main/java/me/river/royaltisapi/RoyaltisApiApplication.java
@@ -0,0 +1,13 @@
+package me.river.royaltisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyaltisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoyaltisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/river/royaltisapi/RoyaltisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.river.royaltisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyaltisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

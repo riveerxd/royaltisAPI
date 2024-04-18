c4ad010c0429e6a26f32397cd80c363397de7461
riveerxd <lukas.hrehor@gmail.com>
2024-04-18 22:34:35 +0200

    updated SocketIOConfig to enable broadcasting

--- src/main/java/me/river/royaltisapi/core/socket/SocketIOConfig.java
+++ src/main/java/me/river/royaltisapi/core/socket/SocketIOConfig.java
@@ -7,24 +7,17 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DataListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import io.netty.handler.codec.http.HttpHeaders;
 import jakarta.annotation.PreDestroy;
-import me.river.royaltisapi.core.UserManager;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @org.springframework.context.annotation.Configuration
 public class SocketIOConfig {
 
     private String SOCKETHOST = "0.0.0.0";
     private int SOCKETPORT = 9090;
-    private final Gson gson = new Gson(); // Initialize Gson instance
-    private SocketIOServer server;
+    public static SocketIOServer server;
 
     @Bean
     public SocketIOServer socketIOServer() {
@@ -50,30 +43,19 @@ public class SocketIOConfig {
             }
         });
 
-        // Add DataListener with content type check
         server.addEventListener("message", String.class, new DataListener<String>() {
             @Override
             public void onData(SocketIOClient client, String data, AckRequest ackRequest) throws Exception {
-                HttpHeaders headers = client.getHandshakeData().getHttpHeaders();
-                String contentType = headers.get("Content-Type");
-                String messageType = headers.get("Message-Type");
-                 if (messageType != null && messageType.equalsIgnoreCase("location-update")) {
-                    UserManager userManager = new UserManager();
-                    try {
-                        // Parse JSON using Gson and log it
-                        JsonObject messageObject = gson.fromJson(data, JsonObject.class);
-                        System.out.println("Client " + client.getSessionId() + " sent message: " + messageObject);
-                        userManager.handleLocationUpdate(client, messageObject);
-                    } catch (Exception e) {
-                        System.err.println("Error parsing JSON message: " + e.getMessage());
-                    }
-                } else {
-                    System.out.println("Client " + client.getSessionId() + " sent message (not JSON): " + data);
-                }
+                return;
             }
         });
 
         return server;
+    }
+
+    public static void broadcastMessage(Object data) {
+        System.out.println("message sent: "+data);
+        server.getBroadcastOperations().sendEvent("broadcastMessage", data);
     }
 
     @PreDestroy

19807c4a62187156890c9dc64b7a2b6080eabe30
riveerxd <lukas.hrehor@gmail.com>
2024-04-23 22:16:42 +0200

    added socketSessionId and rank to User

--- src/main/java/me/river/royaltisapi/core/User.java
+++ src/main/java/me/river/royaltisapi/core/User.java
@@ -1,15 +1,55 @@
 package me.river.royaltisapi.core;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class User {
     private String username;
     private String password;
     private String email;
-
-
-    public User(String username, String password, String email) {
+    private UUID socketSessionId;
+    private Rank rank;
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
+    }
+
+    public User(UUID socketSessionId, Rank rank) {
+        this.socketSessionId = socketSessionId;
+        this.rank = rank;
+    }
+
+    public User(UUID socketSessionId) {
+        this.socketSessionId = socketSessionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(socketSessionId, user.socketSessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(socketSessionId);
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public void setSocketSessionId(UUID socketSessionId) {
+        this.socketSessionId = socketSessionId;
+    }
+
+    public UUID getSocketSessionId() {
+        return socketSessionId;
+    }
+
+    public boolean isAdmin(){
+        return rank == Rank.ADMIN;
     }
 
     public String getUsername() {
@@ -23,4 +63,16 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", socketSessionId='" + socketSessionId + '\'' +
+                ", rank=" + rank +
+                '}';
+    }
 }
+

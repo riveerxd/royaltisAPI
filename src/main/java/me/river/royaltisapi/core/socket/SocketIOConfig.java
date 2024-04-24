567a124db62d60a3b6bb1be61fe48caf2979f3ae
riveerxd <lukas.hrehor@gmail.com>
2024-04-24 20:32:52 +0200

    added userManager bean added lobbyManager bean added server shutdown bean

--- src/main/java/me/river/royaltisapi/core/socket/SocketIOConfig.java
+++ src/main/java/me/river/royaltisapi/core/socket/SocketIOConfig.java
@@ -1,65 +1,69 @@
 package me.river.royaltisapi.core.socket;
 
-import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
-import com.corundumstudio.socketio.listener.DisconnectListener;
+import com.corundumstudio.socketio.*;
 import jakarta.annotation.PreDestroy;
+import me.river.royaltisapi.core.managers.LobbyManager;
+import me.river.royaltisapi.core.managers.UserManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Configuration;
 
-@CrossOrigin(origins = "*")
-@org.springframework.context.annotation.Configuration
+@Configuration
 public class SocketIOConfig {
-
-    private String SOCKETHOST = "0.0.0.0";
-    private int SOCKETPORT = 9090;
-    public static SocketIOServer server;
+    private String socketHost = "0.0.0.0";
+    private int socketPort = 9090;
 
     @Bean
     public SocketIOServer socketIOServer() {
-        Configuration config = new Configuration();
-        config.setHostname(SOCKETHOST);
-        config.setPort(SOCKETPORT);
-        server = new SocketIOServer(config);
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(socketHost);
+        config.setPort(socketPort);
+
+        SocketIOServer server = new SocketIOServer(config);
+
+        server.addEventListener("message", String.class, (client, data, ackRequest) -> {
+            System.out.println("Message: "+data);
+        });
+        server.addConnectListener(client -> userManager().handleClientConnect(client));
+
+        server.addDisconnectListener(client -> userManager().handleClientDisconnect(client));
         server.start();
-
-        server.addConnectListener(new ConnectListener() {
-            @Override
-            public void onConnect(SocketIOClient client) {
-                System.out.println("new user connected with socket " + client.getSessionId());
-            }
-        });
-
-        server.addDisconnectListener(new DisconnectListener() {
-            @Override
-            public void onDisconnect(SocketIOClient client) {
-                client.getNamespace().getAllClients().stream().forEach(data -> {
-                    System.out.println("user disconnected " + data.getSessionId().toString());
-                });
-            }
-        });
-
-        server.addEventListener("message", String.class, new DataListener<String>() {
-            @Override
-            public void onData(SocketIOClient client, String data, AckRequest ackRequest) throws Exception {
-                return;
-            }
-        });
 
         return server;
     }
 
-    public static void broadcastMessage(Object data) {
-        System.out.println("message sent: "+data);
+    public static void broadcastMessage(Object data, SocketIOServer server) {
+        System.out.println("Broadcasting message: " + data);
         server.getBroadcastOperations().sendEvent("broadcastMessage", data);
     }
 
-    @PreDestroy
-    public void stopSocketIOServer() {
-        this.server.stop();
+    @Bean
+    public UserManager userManager() {
+        return new UserManager();
+    }
+
+    @Bean
+    public LobbyManager lobbyManager(){
+        return new LobbyManager();
+    }
+
+    @Bean
+    public SocketIOServerShutdown socketIOServerShutdown(SocketIOServer server) {
+        return new SocketIOServerShutdown(server);
+    }
+
+    static class SocketIOServerShutdown {
+
+        private final SocketIOServer server;
+
+        public SocketIOServerShutdown(SocketIOServer server) {
+            this.server = server;
+        }
+
+        @PreDestroy
+        public void stopSocketIOServer() {
+            if (server != null) {
+                server.stop();
+            }
+        }
     }
 }

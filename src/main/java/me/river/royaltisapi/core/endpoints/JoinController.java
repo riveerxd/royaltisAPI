31628495e762b26d20e4274b14fbf86bdf329c39
riveerxd <lukas.hrehor@gmail.com>
2024-05-03 13:35:30 +0200

    added join endpoint for game joining

---
+++ src/main/java/me/river/royaltisapi/core/endpoints/JoinController.java
@@ -0,0 +1,35 @@
+package me.river.royaltisapi.core.endpoints;
+
+import com.google.gson.Gson;
+import me.river.royaltisapi.core.data.GameId;
+import me.river.royaltisapi.core.data.LobbyCode;
+import me.river.royaltisapi.core.managers.LobbyManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JoinController {
+    @Autowired
+    LobbyManager lobbyManager;
+
+    @PostMapping("/join")
+    public ResponseEntity join(
+            @RequestBody String body
+    ){
+        Gson gson = new Gson();
+        try{
+            LobbyCode lobbyCode = gson.fromJson(body, LobbyCode.class);
+            if (!lobbyManager.doesLobbyExist(lobbyCode)){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid lobby code");
+            }
+            GameId wantedGameId = lobbyManager.getGameIdByLobbyCode(lobbyCode);
+            return ResponseEntity.status(HttpStatus.OK).body(wantedGameId.gameId());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}

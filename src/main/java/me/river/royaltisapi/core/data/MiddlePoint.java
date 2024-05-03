7286bbac7578dd8c1fd9c515af6385d66730341b
riveerxd <lukas.hrehor@gmail.com>
2024-05-03 13:36:09 +0200

    modified MiddlePoint class

--- src/main/java/me/river/royaltisapi/core/data/MiddlePoint.java
+++ src/main/java/me/river/royaltisapi/core/data/MiddlePoint.java
@@ -1,9 +1,15 @@
 package me.river.royaltisapi.core.data;
 
 public class MiddlePoint {
+    private int gameId;
     private Coordinates coordinates;
 
     public MiddlePoint(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public MiddlePoint(int gameId, Coordinates coordinates) {
+        this.gameId = gameId;
         this.coordinates = coordinates;
     }
 
@@ -11,7 +17,23 @@ public class MiddlePoint {
         return coordinates;
     }
 
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
     public void setCoordinates(Coordinates coordinates) {
         this.coordinates = coordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "MiddlePoint{" +
+                "gameId=" + gameId +
+                ", coordinates=" + coordinates +
+                '}';
     }
 }

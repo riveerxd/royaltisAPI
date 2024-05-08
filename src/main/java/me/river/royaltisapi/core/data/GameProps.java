befd79a40e6076ba655ef8b8ee76a90dfa4387eb
riveerxd <lukas.hrehor@gmail.com>
2024-05-09 00:21:37 +0200

    updated GameProps class added lobby code to GameProps class

--- src/main/java/me/river/royaltisapi/core/data/GameProps.java
+++ src/main/java/me/river/royaltisapi/core/data/GameProps.java
@@ -4,12 +4,15 @@ public class GameProps {
     private int gameId;
     private int count;
     private int interval;
+    private String lobbyCode;
 
-    public GameProps(int gameId, int count, int interval) {
+    public GameProps(int gameId, int count, int interval, LobbyCode lobbyCode) {
         this.gameId = gameId;
         this.count = count;
         this.interval = interval;
+        this.lobbyCode = lobbyCode.toString();
     }
+
 
     public int getGameId() {
         return gameId;
@@ -21,5 +24,9 @@ public class GameProps {
 
     public int getInterval() {
         return interval;
+    }
+
+    public LobbyCode getLobbyCode() {
+        return new LobbyCode(this.lobbyCode);
     }
 }

c7fc5423903734172c5088236c63eaacbbc92e3f
riveerxd <lukas.hrehor@gmail.com>
2024-05-13 09:10:22 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/data/records/GameDetails.java
+++ src/main/java/me/river/royaltisapi/core/data/records/GameDetails.java
@@ -1,3 +1,8 @@
 package me.river.royaltisapi.core.data.records;
 
+/**
+ * Represents game details.
+ * @param gameId The unique identifier of the game.
+ * @param gameName The name of the game.
+ */
 public record GameDetails(int gameId, String gameName) {}

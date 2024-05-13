288a8d6f5dd366b32f9781eee0af80afeac571bb
riveerxd <lukas.hrehor@gmail.com>
2024-05-13 09:10:12 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/data/records/GameProps.java
+++ src/main/java/me/river/royaltisapi/core/data/records/GameProps.java
@@ -1,6 +1,17 @@
 package me.river.royaltisapi.core.data.records;
 
+/**
+ * Represents the properties of a game.
+ * @param gameId The unique identifier of the game.
+ * @param count The number of border moves.
+ * @param interval The interval between border move.
+ * @param lobbyCode The lobby code of the game.
+ */
 public record GameProps(int gameId, int count, int interval, String lobbyCode) {
+    /**
+     * Returns LobbyCode instance of the lobby code string.
+     * @return The unique identifier of the game.
+     */
     public LobbyCode getLobbyCode() {
         return new LobbyCode(this.lobbyCode);
     }

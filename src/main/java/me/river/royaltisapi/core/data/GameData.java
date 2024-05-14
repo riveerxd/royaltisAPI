6ce1db95cf131118a9c0534f461b8c4abe7a78d1
riveerxd <lukas.hrehor@gmail.com>
2024-05-14 23:08:07 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/data/GameData.java
+++ src/main/java/me/river/royaltisapi/core/data/GameData.java
@@ -1,27 +1,59 @@
 package me.river.royaltisapi.core.data;
 
+import me.river.royaltisapi.core.data.records.Border;
+
 import java.util.ArrayList;
 
+/**
+ * GameData represents a game's data, including its borders, loot boxes, and other metadata.
+ */
 public class GameData {
+    /**
+     * The list of borders in this game.
+     */
     private ArrayList<Border> borders;
+
+    /**
+     * The list of loot boxes in this game.
+     */
     private ArrayList<LootBox> lootboxes;
+
+    /**
+     * The name of the game.
+     */
     private String gameName;
+
+    /**
+     * The unique identifier for this game.
+     */
     private int gameID;
+
+    /**
+     * The middle point of the game board.
+     */
     private MiddlePoint middlePoint;
 
+    /**
+     * Creates a new GameData instance with the specified borders, loot boxes, game name, and middle point.
+     * @param borders The list of borders in this game.
+     * @param lootboxes The list of loot boxes in this game.
+     * @param gameName The name of the game.
+     * @param middlePoint The middle point of the game board.
+     */
     public GameData(ArrayList<Border> borders, ArrayList<LootBox> lootboxes, String gameName, MiddlePoint middlePoint) {
+        this.borders = borders;
+        this.lootboxes = lootboxes;
+        this.gameName = gameName;
         this.middlePoint = middlePoint;
-        this.gameName = gameName;
-        this.lootboxes = lootboxes;
-        this.borders = borders;
     }
 
-    public GameData(ArrayList<Border> borders, ArrayList<LootBox> lootboxes, String gameName) {
-        this.borders = borders;
-        this.lootboxes = lootboxes;
-        this.gameName = gameName;
-    }
-
+    /**
+     * Creates a new GameData instance with the specified borders, loot boxes, game name, and middle point.
+     * @param borders The list of borders in this game.
+     * @param lootboxes The list of loot boxes in this game.
+     * @param gameName The name of the game.
+     * @param middlePoint The middle point of the game board.
+     */
     public GameData(ArrayList<Border> borders, ArrayList<LootBox> lootboxes, MiddlePoint middlePoint, String gameName) {
         this.borders = borders;
         this.lootboxes = lootboxes;
@@ -29,31 +61,58 @@ public class GameData {
         this.gameName = gameName;
     }
 
-
-    public void setGameID(int gameID) {
-        this.gameID = gameID;
-    }
-
+    /**
+     * Returns the list of borders in this game.
+     * @return The list of borders in this game.
+     */
     public ArrayList<Border> getBorders() {
         return borders;
     }
 
+    /**
+     * Returns the list of loot boxes in this game.
+     * @return The list of loot boxes in this game.
+     */
     public ArrayList<LootBox> getLootboxes() {
         return lootboxes;
     }
 
+    /**
+     * Returns the name of the game.
+     * @return The name of the game.
+     */
     public String getGameName() {
         return gameName;
     }
 
-    public MiddlePoint getMiddlePoint() {
-        return middlePoint;
-    }
-
+    /**
+     * Returns the unique identifier for this game.
+     * @return The unique identifier for this game.
+     */
     public int getGameID() {
         return gameID;
     }
 
+    /**
+     * Returns the middle point of the game board.
+     * @return The middle point of the game board.
+     */
+    public MiddlePoint getMiddlePoint() {
+        return middlePoint;
+    }
+
+    /**
+     * Sets the unique identifier for this game.
+     * @param gameID The unique identifier for this game.
+     */
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    /**
+     * Game data to string
+     * @return the string.
+     */
     @Override
     public String toString() {
         return "GameData{" +

44c928ef6aa292847800017be8215f1251c14185
riveerxd <lukas.hrehor@gmail.com>
2024-05-14 23:06:29 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/db/DBConnector.java
+++ src/main/java/me/river/royaltisapi/core/db/DBConnector.java
@@ -5,8 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static me.river.royaltisapi.core.data.AccessKeys.*;
-
+/**
+ * Connects to the database.
+ */
 public class DBConnector {
+    /**
+     * Gets a connection to the database.
+     *
+     * @return the connection
+     * @throws SQLException the sql exception
+     * @throws ClassNotFoundException the class not found exception
+     */
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName(DRIVER_CLASS);
         return DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);

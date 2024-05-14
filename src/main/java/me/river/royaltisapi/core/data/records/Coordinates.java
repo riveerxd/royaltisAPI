b004325edc486ff0c324d0949616c4679d33d513
riveerxd <lukas.hrehor@gmail.com>
2024-05-14 23:05:26 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/data/records/Coordinates.java
+++ src/main/java/me/river/royaltisapi/core/data/records/Coordinates.java
@@ -1,3 +1,8 @@
 package me.river.royaltisapi.core.data.records;
 
+/**
+ * Represents a set of coordinates.
+ * @param latitude
+ * @param longitude
+ */
 public record Coordinates(double latitude, double longitude) {}

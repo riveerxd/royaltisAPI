e4df6e814c7ef02a64a671c9c5bee32e37989b11
riveerxd <lukas.hrehor@gmail.com>
2024-05-15 22:23:44 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/endpoints/JoinController.java
+++ src/main/java/me/river/royaltisapi/core/endpoints/JoinController.java
@@ -11,11 +11,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for joining a game.
+ */
 @RestController
 public class JoinController {
+    /**
+     * The lobby manager.
+     */
     @Autowired
     private LobbyManager lobbyManager;
 
+    /**
+     * The Gson instance.
+     */
+    private Gson gson = new Gson();
+
+    /**
+     * Responds with game id of wanted game based on lobby code.
+     *
+     * @param body the body
+     * @return the response entity
+     */
     @PostMapping("/join")
     public ResponseEntity join(
             @RequestBody String body

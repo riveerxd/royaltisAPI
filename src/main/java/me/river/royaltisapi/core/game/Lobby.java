d1a84337efdb018d872ca4abc3f983ed2549b8c4
riveerxd <lukas.hrehor@gmail.com>
2024-05-15 22:23:56 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/game/Lobby.java
+++ src/main/java/me/river/royaltisapi/core/game/Lobby.java
@@ -7,39 +7,82 @@ import me.river.royaltisapi.core.data.LootBox;
 import java.util.HashSet;
 import java.util.Objects;
 
+/**
+ * Represents a lobby.
+ */
 public class Lobby {
+    /**
+     * The lobby code.
+     */
     private LobbyCode lobbyCode;
+    /**
+     * The game id.
+     */
     private GameId gameId;
+    /**
+     * The online users.
+     */
     private HashSet<User> onlineUsers = new HashSet<>();
+    /**
+     * The removed items.
+     */
     private HashSet<LootBox.Item> removedItems = new HashSet<>();
 
+    /**
+     * Add item to removed items.
+     *
+     * @param item the item
+     * @return the boolean
+     */
     public boolean addItem(LootBox.Item item){
         return removedItems.add(item);
     }
 
-    public boolean removeItem(LootBox.Item item){
-        return removedItems.remove(item);
-    }
-
+    /**
+     * Connects a user.
+     *
+     * @param user the user
+     * @return connected or not
+     */
     public boolean connectUser(User user){
         onlineUsers.add(user);
-        System.out.println("User "+user.getSocketSessionId()+" connected to lobby "+lobbyCode);
-        System.out.println("online users in lobby" + lobbyCode+ this.onlineUsers);
+        System.out.println(user.isAdmin() ? "Admin ": "User "+user.getSocketSessionId()+" connected to lobby "+lobbyCode);
         return true;
     }
 
+    /**
+     * Disconnects a user.
+     *
+     * @param user the user
+     * @return disconnected or not
+     */
     public boolean disconnectUser(User user){
         return onlineUsers.remove(user);
     }
 
+    /**
+     * Gets online users.
+     *
+     * @return the online users
+     */
     public HashSet<User> getOnlineUsers() {
         return onlineUsers;
     }
 
+    /**
+     * Gets removed items.
+     *
+     * @return the removed items
+     */
     public HashSet<LootBox.Item> getRemovedItems() {
         return removedItems;
     }
 
+    /**
+     * Lobby equals.
+     * @param o the object
+     * @return equals or not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -48,28 +91,58 @@ public class Lobby {
         return Objects.equals(lobbyCode, lobby.lobbyCode);
     }
 
+    /**
+     * Sets lobby code.
+     *
+     * @param lobbyCode the lobby code
+     */
     public void setLobbyCode(LobbyCode lobbyCode) {
         this.lobbyCode = lobbyCode;
     }
 
+    /**
+     * Hash code int.
+     *
+     * @return the int
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(lobbyCode);
     }
 
+    /**
+     * Instantiates a new Lobby.
+     *
+     * @param lobbyCode the lobby code
+     * @param gameId the game id
+     */
     public Lobby(LobbyCode lobbyCode, GameId gameId) {
         this.lobbyCode = lobbyCode;
         this.gameId = gameId;
     }
 
+    /**
+     * Gets lobby code.
+     *
+     * @return the lobby code
+     */
     public LobbyCode getLobbyCode() {
         return lobbyCode;
     }
 
+    /**
+     * Gets game id.
+     *
+     * @return the game id
+     */
     public GameId getGameId() {
         return gameId;
     }
 
+    /**
+     * Lobby to string.
+     * @return the string
+     */
     @Override
     public String toString() {
         return "Lobby{" +

ef88a2e78c79526531f834e412aee82051f4af7d
riveerxd <lukas.hrehor@gmail.com>
2024-05-27 11:52:26 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/managers/UserManager.java
+++ src/main/java/me/river/royaltisapi/core/managers/UserManager.java
@@ -1,14 +1,14 @@
 package me.river.royaltisapi.core.managers;
 
-import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.BroadcastOperations;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.google.gson.Gson;
-import me.river.royaltisapi.core.Lobby;
-import me.river.royaltisapi.core.Rank;
-import me.river.royaltisapi.core.User;
-import me.river.royaltisapi.core.data.LobbyCode;
+import me.river.royaltisapi.core.data.records.Coordinates;
+import me.river.royaltisapi.core.game.Lobby;
+import me.river.royaltisapi.core.data.Rank;
+import me.river.royaltisapi.core.game.User;
+import me.river.royaltisapi.core.data.records.LobbyCode;
 import me.river.royaltisapi.core.data.LootBox;
 import me.river.royaltisapi.core.db.LoginCheck;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +17,39 @@ import org.springframework.context.annotation.DependsOn;
 import java.util.HashSet;
 import java.util.UUID;
 
+/**
+ * The User manager.
+ */
 @DependsOn("socketIOServer")
 public class UserManager {
-    private HashSet<User> users = new HashSet<>();
+    /**
+     * The server.
+     */
     @Autowired
     private SocketIOServer server;
 
+    /**
+     * The lobby manager.
+     */
     @Autowired
     private LobbyManager lobbyManager;
 
+    /**
+     * The users.
+     */
+    private HashSet<User> users = new HashSet<>();
+
+    /**
+     * The Gson instance.
+     */
     private Gson gson = new Gson();
 
-
+    /**
+     * Remove user by session id.
+     * Removes the user based on the session id.
+     *
+     * @param sessionId the session id
+     */
     public boolean removeUserBySessionId(UUID sessionId) {
         for (User user : users) {
             if (user.getSocketSessionId().toString().equals(sessionId.toString())) {
@@ -38,9 +59,15 @@ public class UserManager {
         throw new RuntimeException("User not found");
     }
 
+    /**
+     * Gets user by session id.
+     * Returns the user based on the session id.
+     *
+     * @param sessionId the session id
+     * @return the user by session id
+     */
     public User getUserBySessionId(UUID sessionId) {
         for (User user : users) {
-            System.out.println("comparing "+user.getSocketSessionId()+" with "+sessionId);
             if (user.getSocketSessionId().toString().equals(sessionId.toString())) {
                 return user;
             }
@@ -48,6 +75,70 @@ public class UserManager {
         throw new RuntimeException("User not found");
     }
 
+    /**
+     * Handle client disconnect.
+     * Disconnects the client from the lobby based on the session id.
+     *
+     * @param client the client
+     */
+    public void handleClientDisconnect(SocketIOClient client) {
+        try{
+            lobbyManager.disconnectFromLobby(getUserBySessionId(client.getSessionId()));
+            removeUserBySessionId(client.getSessionId());
+            System.out.println("User disconnected " + client.getSessionId().toString());
+        }catch (Exception e){
+            System.out.println("Error while disconnecting user "+client.getSessionId()+": "+e.getMessage());
+        }
+    }
+
+    /**
+     * Handle item remove.
+     * Parses the json data and removes the item from the lobby.
+     *
+     * @param client the client
+     * @param data the data
+     */
+    public void handleItemRemove(SocketIOClient client, Object data){
+        try{
+            System.out.println("data: "+ data);
+            LootBox.Item item = gson.fromJson(data.toString(), LootBox.Item.class);
+
+            Lobby lobby = lobbyManager.getLobbyByClient(client);
+            lobby.addItem(item);
+            for (User user : lobby.getOnlineUsers()){
+                user.getClient().sendEvent("removed_items", gson.toJson(lobby.getRemovedItems()));
+                System.out.println("sending "+gson.toJson(lobby.getRemovedItems())+" to "+user.getSocketSessionId());
+            }
+        } catch (Exception e){
+            System.out.println("Error while removing item: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void handleLocationUpdate(SocketIOClient client, Object data){
+        try{
+            System.out.println("data: "+data);
+
+            Coordinates coords = gson.fromJson(data.toString(), Coordinates.class);
+
+            Lobby lobby = lobbyManager.getLobbyByClient(client);
+            lobby.getOnlineUserByClient(client).setCoordinates(coords);
+
+            for (User user : lobby.getOnlineUsers()){
+                user.getClient().sendEvent("user_locations", gson.toJson(lobby.getOnlineUsers()));
+            }
+        } catch (Exception e){
+            System.err.println("Error while updating user location: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handle client connect.
+     * Checks if the client is authorized and connects them to the lobby either as an admin or a user, then sends the updated player list to all clients.
+     *
+     * @param client the client
+     */
     public void handleClientConnect(SocketIOClient client) {
         BroadcastOperations broadcastOperations = server.getBroadcastOperations();
         final UUID sessionId = client.getSessionId();
@@ -85,41 +176,12 @@ public class UserManager {
                 User user = new User(client, Rank.USER);
                 lobbyManager.connectToLobby(user, lobbyCode);
                 users.add(user);
-                System.out.println("Client connected " + client.getSessionId());
             }catch (Exception r) {
                 client.disconnect();
                 System.out.println("Disconnected user " + client.getSessionId() + " lobby code invalid: "+client.getHandshakeData().getHttpHeaders().get("X-LobbyCode"));
             }finally {
                 broadcastOperations.sendEvent("playerUpdate", gson.toJson(users));
             }
-        }
-        System.out.println("all connected users: "+users);
-    }
-    public void handleClientDisconnect(SocketIOClient client) {
-        try{
-            lobbyManager.disconnectFromLobby(getUserBySessionId(client.getSessionId()));
-            System.out.println(removeUserBySessionId(client.getSessionId()));
-            System.out.println(users);
-            System.out.println("User disconnected " + client.getSessionId().toString());
-        }catch (Exception e){
-            System.out.println("Error while disconnecting user "+client.getSessionId()+": "+e.getMessage());
-    }
-    }
-
-    public void handleItemRemove(SocketIOClient client, Object data, AckRequest ack){
-        try{
-            System.out.println("data: "+ data);
-            LootBox.Item item = gson.fromJson(data.toString(), LootBox.Item.class);
-
-            Lobby lobby = lobbyManager.getLobbyByClient(client);
-            lobby.addItem(item);
-            for (User user : lobby.getOnlineUsers()){
-                user.getClient().sendEvent("removed_items", gson.toJson(lobby.getRemovedItems()));
-                System.out.println("sending "+gson.toJson(lobby.getRemovedItems())+" to "+user.getSocketSessionId());
-            }
-        } catch (Exception e){
-            System.out.println("Error while removing item: "+e.getMessage());
-            e.printStackTrace();
         }
     }
 }

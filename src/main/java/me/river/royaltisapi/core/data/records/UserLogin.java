b3d09f7d1e54306e58a776867607bf9e21cfd98f
riveerxd <lukas.hrehor@gmail.com>
2024-05-27 11:52:11 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/data/records/UserLogin.java
+++ src/main/java/me/river/royaltisapi/core/data/records/UserLogin.java
@@ -1,4 +1,9 @@
 package me.river.royaltisapi.core.data.records;
 
+/**
+ * Represents a user login.
+ * @param username The username of the user.
+ * @param password The password of the user.
+ */
 public record UserLogin(String username, String password) {
 }

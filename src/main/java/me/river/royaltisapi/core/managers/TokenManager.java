4a48f6e53c361abfef02e5646fbf00fdf2bbc1fe
riveerxd <lukas.hrehor@gmail.com>
2024-05-27 11:53:30 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/managers/TokenManager.java
+++ src/main/java/me/river/royaltisapi/core/managers/TokenManager.java
@@ -1,19 +1,40 @@
 package me.river.royaltisapi.core.managers;
 
-import me.river.royaltisapi.core.User;
+import me.river.royaltisapi.core.game.User;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
+/**
+ * The Token manager.
+ */
 public class TokenManager {
+    /**
+     * The Algorithm.
+     */
     private static final String ALGORITHM = "AES";
+    /**
+     * The Key.
+     */
     private static final String KEY = "Jd7w92uz1g5Djd82";
 
+    /**
+     * Gets user token.
+     *
+     * @param user the user
+     * @return the user token
+     */
     public static String getUserToken(User user){
         return encrypt(user.getUsername()+";"+user.getPassword());
     }
 
+    /**
+     * Gets user from token.
+     *
+     * @param token the token
+     * @return the user from token
+     */
     public static User getUserFromToken(String token){
         String[] decrypted = decrypt(token).split(";");
         return new User(decrypted[0], decrypted[1]);

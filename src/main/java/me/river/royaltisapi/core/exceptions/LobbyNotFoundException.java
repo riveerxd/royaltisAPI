3c3b9d72d8f015358c321d6481a288f260e1da9b
riveerxd <lukas.hrehor@gmail.com>
2024-05-29 23:11:03 +0200

    added LobbyNotFoundException

---
+++ src/main/java/me/river/royaltisapi/core/exceptions/LobbyNotFoundException.java
@@ -0,0 +1,7 @@
+package me.river.royaltisapi.core.exceptions;
+
+public class LobbyNotFoundException extends Exception{
+    public LobbyNotFoundException(String message) {
+        super(message);
+    }
+}

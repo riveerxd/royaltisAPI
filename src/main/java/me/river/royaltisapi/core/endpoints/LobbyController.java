d6802b2c92e03d60796f5690304bf75393f3d297
riveerxd <lukas.hrehor@gmail.com>
2024-05-29 23:10:40 +0200

    updated exception handling

--- src/main/java/me/river/royaltisapi/core/endpoints/LobbyController.java
+++ src/main/java/me/river/royaltisapi/core/endpoints/LobbyController.java
@@ -22,6 +22,10 @@ public class LobbyController {
      * The lobby manager.
      */
     private LobbyManager lobbyManager;
+    /**
+     * The Gson instance.
+     */
+    private Gson gson = new Gson();
 
     /**
      * Instantiates a new Lobby controller.
@@ -34,36 +38,36 @@ public class LobbyController {
     }
 
     /**
-     * The Gson instance.
-     */
-    private Gson gson = new Gson();
-    /**
      * Tries to create a lobby, returns the lobby code of the created lobby.
      *
      * @param authorizationHeader the authorization header
-     * @param body the body
+     * @param body                the body
      * @return the response entity
      */
     @PostMapping("/createlobby")
     public ResponseEntity<String> createLobby(
-            @RequestHeader(value = "Authorization", required = false) String authorizationHeader,
+            @RequestHeader(value = "Authorization") String authorizationHeader,
             @RequestBody String body
-    ){
-        if (LoginCheck.checkLoginToken(authorizationHeader)){
-            try{
-                GameId gameId = gson.fromJson(body, GameId.class);
-                if (DbUtils.doesGameExist(gameId)){
-                    String lobbyCode = lobbyManager.createLobby(gameId);
-                    return ResponseEntity.status(HttpStatus.CREATED).body(lobbyCode);
-                }else{
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Game id does not exist");
+    ) {
+        try {
+            if (LoginCheck.checkLoginToken(authorizationHeader)) {
+                try {
+                    GameId gameId = gson.fromJson(body, GameId.class);
+                    if (DbUtils.doesGameExist(gameId)) {
+                        String lobbyCode = lobbyManager.createLobby(gameId);
+                        return ResponseEntity.status(HttpStatus.CREATED).body(lobbyCode);
+                    } else {
+                        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Game id does not exist");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while creating lobby");
                 }
-            }catch (Exception e){
-                e.printStackTrace();
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while creating lobby");
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
             }
-        }else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
@@ -71,24 +75,28 @@ public class LobbyController {
      * Removes a lobby.
      *
      * @param authorizationHeader the authorization header
-     * @param body the body
+     * @param body                the body
      * @return the response entity
      */
     @DeleteMapping("/removelobby")
     public ResponseEntity<String> removeLobby(
             @RequestHeader(value = "Authorization", required = false) String authorizationHeader,
             @RequestBody String body
-    ){
-        if (LoginCheck.checkLoginToken(authorizationHeader)){
-            try{
-                LobbyCode lobbyCode = gson.fromJson(body, LobbyCode.class);
-                lobbyManager.removeLobby(lobbyCode);
-                return ResponseEntity.status(HttpStatus.OK).body("Lobby removed");
-            }catch (Exception e){
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while removing lobby");
+    ) {
+        try {
+            if (LoginCheck.checkLoginToken(authorizationHeader)) {
+                try {
+                    LobbyCode lobbyCode = gson.fromJson(body, LobbyCode.class);
+                    lobbyManager.removeLobby(lobbyCode);
+                    return ResponseEntity.status(HttpStatus.OK).body("Lobby removed");
+                } catch (Exception e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while removing lobby");
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
             }
-        }else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 

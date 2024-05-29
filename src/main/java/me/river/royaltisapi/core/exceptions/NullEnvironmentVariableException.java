6ca9d221f0b7ca966e7503b398cfd16e680954f3
riveerxd <lukas.hrehor@gmail.com>
2024-05-29 23:11:17 +0200

    added NullEnvironmentVariableException

---
+++ src/main/java/me/river/royaltisapi/core/exceptions/NullEnvironmentVariableException.java
@@ -0,0 +1,7 @@
+package me.river.royaltisapi.core.exceptions;
+
+public class NullEnvironmentVariableException extends Exception{
+    public NullEnvironmentVariableException(String message) {
+        super(message);
+    }
+}

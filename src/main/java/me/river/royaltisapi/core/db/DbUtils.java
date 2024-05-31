fb3fda70508717d5393bd47bc56397159f66ff32
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 11:57:45 +0200

    reworked logging system changed documentation

--- src/main/java/me/river/royaltisapi/core/db/DbUtils.java
+++ src/main/java/me/river/royaltisapi/core/db/DbUtils.java
@@ -2,6 +2,8 @@ package me.river.royaltisapi.core.db;
 
 import me.river.royaltisapi.core.data.records.GameId;
 import me.river.royaltisapi.core.exceptions.NullEnvironmentVariableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,22 +11,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Database utilities.
+ * Provides utility functions for database operations.
  */
 public class DbUtils {
+    private static final Logger logger = LoggerFactory.getLogger(DbUtils.class);
+
     /**
-     * Checks if a game exists.
+     * Checks if a game with the given ID exists in the database.
      *
-     * @param gameId the game id
-     * @return true if the game exists, false otherwise
+     * @param gameId The ID of the game to check.
+     * @return True if the game exists, false otherwise.
+     * @throws SQLException                  If a database access error occurs.
+     * @throws ClassNotFoundException         If the JDBC driver class cannot be found.
+     * @throws NullEnvironmentVariableException If a required environment variable is not set.
      */
     public static boolean doesGameExist(GameId gameId) throws SQLException, ClassNotFoundException, NullEnvironmentVariableException {
-        Connection connection = DBConnector.getConnection();
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Games WHERE id = ?");
-        statement.setInt(1, Integer.parseInt(String.valueOf(gameId.gameId())));
-        ResultSet rs = statement.executeQuery();
-        boolean is = rs.next();
-        connection.close();
-        return is;
+        logger.info("Checking if game with ID {} exists in database", gameId.gameId());
+        try (Connection connection = DBConnector.getConnection()) {
+            logger.debug("Executing SQL query: SELECT * FROM Games WHERE id = {}", gameId.gameId());
+            String query = "SELECT * FROM Games WHERE id = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setInt(1, Integer.parseInt(String.valueOf(gameId.gameId())));
+                try (ResultSet rs = statement.executeQuery()) {
+                    boolean exists = rs.next();
+                    logger.info("Game with ID {} exists: {}", gameId.gameId(), exists);
+                    return exists;
+                }
+            }
+        }
     }
 }

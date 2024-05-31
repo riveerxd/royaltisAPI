cbe15fe440feda6158907f3d2dd90e8b4fcb086c
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 11:59:54 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/exceptions/UserNotFoundException.java
+++ src/main/java/me/river/royaltisapi/core/exceptions/UserNotFoundException.java
@@ -1,6 +1,14 @@
 package me.river.royaltisapi.core.exceptions;
 
-public class UserNotFoundException extends Exception{
+/**
+ * This exception is thrown when a requested user is not found in the database.
+ */
+public class UserNotFoundException extends Exception {
+    /**
+     * Constructs a new UserNotFoundException with the specified detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     */
     public UserNotFoundException(String message) {
         super(message);
     }

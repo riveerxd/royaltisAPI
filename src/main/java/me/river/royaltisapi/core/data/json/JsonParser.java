83897e52ca80464f45a248e6b60ae29d837efe15
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 11:59:07 +0200

    reworked logging system changed documentation

--- src/main/java/me/river/royaltisapi/core/data/json/JsonParser.java
+++ src/main/java/me/river/royaltisapi/core/data/json/JsonParser.java
@@ -4,17 +4,24 @@ import com.google.gson.Gson;
 import me.river.royaltisapi.core.data.*;
 import me.river.royaltisapi.core.data.records.Border;
 import me.river.royaltisapi.core.data.records.Coordinates;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A utility class for parsing game data from JSON format.
+ */
 public class JsonParser {
+    private static final Logger logger = LogManager.getLogger(JsonParser.class);
+
     /**
-     * This method takes in a string of JSON data and parses it into an instance of the GameData class.
+     * Parses a JSON string into a {@link GameData} object.
      *
-     * @param jsonData the input JSON data
-     * @return an instance of the GameData class containing the parsed data
+     * @param jsonData the JSON string containing game data
+     * @return the parsed {@link GameData} object
      */
     public static GameData parseJsonToGameData(String jsonData) {
         Gson gson = new Gson();
@@ -27,8 +34,8 @@ public class JsonParser {
         String gameName = "not set";
 
         if (data instanceof List) {
+            logger.info("Parsing game data from JSON...");
             for (Object element : data) {
-                System.out.println(element);
                 if (element instanceof List) {
                     for (Object innerElement : (List) element) {
                         if (innerElement instanceof Map) {
@@ -41,6 +48,7 @@ public class JsonParser {
                                     Map<String, Double> borderCoordsMap = (Map<String, Double>) elementMap.get("coords");
                                     Border border = new Border(borderId, type, new Coordinates(borderCoordsMap.get("latitude"), borderCoordsMap.get("longitude")));
                                     borders.add(border);
+                                    logger.debug("Parsed border: {}", border);
                                     break;
 
                                 case "lootbox":
@@ -56,25 +64,31 @@ public class JsonParser {
                                     }
                                     LootBox lootBox = new LootBox(lootboxId, type, new Coordinates(lootboxCoordsMap.get("latitude"), lootboxCoordsMap.get("longitude")), items);
                                     lootboxes.add(lootBox);
+                                    logger.debug("Parsed lootbox: {}", lootBox);
                                     break;
 
                                 case "mapCenter":
                                     Map<String, Double> mapCenterCoordMap = (Map<String, Double>) elementMap.get("coords");
                                     middlePoint = new MiddlePoint(new Coordinates(mapCenterCoordMap.get("latitude"), mapCenterCoordMap.get("longitude")));
+                                    logger.debug("Parsed map center: {}", middlePoint);
                                     break;
 
                                 case "gameName":
                                     String gameNameValue = String.valueOf(elementMap.get("value"));
                                     gameName = gameNameValue;
+                                    logger.debug("Parsed game name: {}", gameName);
                                     break;
-                            }
 
+                                default:
+                                    logger.warn("Unknown element type: {}", type);
+                            }
                         }
                     }
                 }
             }
+            logger.info("Finished parsing game data.");
         }
 
-        return new GameData(borders, lootboxes, middlePoint, gameName);
+        return new GameData(borders, lootboxes, gameName, middlePoint);
     }
 }

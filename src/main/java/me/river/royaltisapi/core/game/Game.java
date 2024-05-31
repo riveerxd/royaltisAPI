aa8bd21f71ccdf6cd0f007b1b5ed6a34d331c07f
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 11:57:54 +0200

    reworked logging system changed documentation

--- src/main/java/me/river/royaltisapi/core/game/Game.java
+++ src/main/java/me/river/royaltisapi/core/game/Game.java
@@ -3,23 +3,30 @@ package me.river.royaltisapi.core.game;
 import me.river.royaltisapi.core.data.records.Border;
 import me.river.royaltisapi.core.data.records.Coordinates;
 import me.river.royaltisapi.core.data.MiddlePoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
 /**
- * The game logic.
+ * This class provides utility methods related to game logic.
  */
 public class Game {
+    private static final Logger logger = LoggerFactory.getLogger(Game.class);
+
     /**
-     * Moves the borders towards the middle.
+     * Moves the borders of a game towards its middle point.
      *
-     * @param borders the borders
-     * @param middlePoint the middle point
-     * @param updateCount the update count
-     * @return the updated borders
+     * @param borders The original list of borders.
+     * @param middlePoint The middle point of the game.
+     * @param updateCount The number of updates remaining in the game.
+     * @return A new list of borders with updated coordinates.
      */
     public static ArrayList<Border> moveBordersTowardsMiddle(ArrayList<Border> borders, MiddlePoint middlePoint, int updateCount) {
+        logger.debug("Moving borders towards middle point. Update count: {}", updateCount);
+
         if (updateCount <= 0) {
+            logger.warn("Invalid update count: {}. Returning original borders.", updateCount);
             return new ArrayList<>(borders);
         }
 
@@ -43,6 +50,8 @@ public class Game {
             Coordinates updatedCoords = new Coordinates(newLat, newLong);
             Border updatedBorder = new Border(border.id(), border.type(), updatedCoords);
             updatedBorders.add(updatedBorder);
+
+            logger.debug("Border {} moved to: ({}, {})", border.id(), newLat, newLong);
         }
 
         return updatedBorders;

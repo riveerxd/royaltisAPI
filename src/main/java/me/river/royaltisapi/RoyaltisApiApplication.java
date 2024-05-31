ebb6966ee917a9e0bb957a3360de8bfdb87b0881
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 12:00:14 +0200

    reworked logging system changed documentation

--- src/main/java/me/river/royaltisapi/RoyaltisApiApplication.java
+++ src/main/java/me/river/royaltisapi/RoyaltisApiApplication.java
@@ -1,12 +1,27 @@
 package me.river.royaltisapi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * The main entry point for the Royaltis API application.
+ */
 @SpringBootApplication
 public class RoyaltisApiApplication {
+    private static final Logger logger = LogManager.getLogger(RoyaltisApiApplication.class);
 
+    /**
+     * The main method that starts the Spring Boot application.
+     *
+     * @param args The command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
-        SpringApplication.run(RoyaltisApiApplication.class, args);
+        logger.info("Starting Royaltis API application...");
+        ConfigurableApplicationContext context = SpringApplication.run(RoyaltisApiApplication.class, args);
+        logger.info("Server port: {}", context.getEnvironment().getProperty("server.port"));
+        logger.info("Royaltis API application started successfully.");
     }
 }

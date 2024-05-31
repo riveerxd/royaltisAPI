11bc769e928389370c5b72f586f2e82d3b338875
riveerxd <lukas.hrehor@gmail.com>
2024-05-31 11:59:49 +0200

    added documentation

--- src/main/java/me/river/royaltisapi/core/exceptions/NullEnvironmentVariableException.java
+++ src/main/java/me/river/royaltisapi/core/exceptions/NullEnvironmentVariableException.java
@@ -1,6 +1,15 @@
 package me.river.royaltisapi.core.exceptions;
 
-public class NullEnvironmentVariableException extends Exception{
+/**
+ * This exception is thrown when a required environment variable is null.
+ */
+public class NullEnvironmentVariableException extends Exception {
+
+    /**
+     * Constructs a new NullEnvironmentVariableException with the specified detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+     */
     public NullEnvironmentVariableException(String message) {
         super(message);
     }
